4ed83ea26974fdbace0248038788db9a23dcdb98
Djordje Lelek <djordje.lelek@gmail.com>
2021-01-26 00:41:09 +0100

    1.install materialUI and bootstrap; 2.add footer-icons from MUI and owners paragraph

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Layout from "./Layout/Layout";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Layout>
+        <header>header</header>
+        <main>main</main>
+        <Footer />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classes from "./Footer.module.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
+const footer = () => (
+  <footer className={classes.Footer}>
+    <div className={classes.Icons}>
+      <a
+        href="https://www.linkedin.com/in/djordje-lelek-3580b7193/"
+        rel="noopener noreferrer"
+        target="_blank"
+        className={classes.LinedIn}
+      >
+        <LinkedInIcon fontSize="large" />
+      </a>
+      <a
+        href="https://github.com/djordjelelek"
+        className={classes.GitHub}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <GitHubIcon fontSize="large" />
+      </a>
+    </div>
+    <p className={classes.Owner}>©2021 Djordje Lelek</p>
+  </footer>
+);
+export default footer;
