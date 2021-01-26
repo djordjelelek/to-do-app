14c1e324f9632d1e52967668e0e609ad7cf2ec50
Djordje Lelek <djordje.lelek@gmail.com>
2021-01-26 21:58:22 +0100

    reorganize code; delete header, main use all avalible space

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <Header />
+        {/* <Header /> */}
+        {/* <main>main</main> */}
         <Main />
         <Footer />
       </Layout>
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,5 +1,8 @@
 import React from "react";
+import classes from "./Layout.module.css";
 
-const layout = (props) => <div>{props.children}</div>;
+const layout = (props) => (
+  <div className={classes.Layout}>{props.children}</div>
+);
 
 export default layout;
