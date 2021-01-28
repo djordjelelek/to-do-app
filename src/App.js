c498d7c9f273b110911d01a9de3bbd25db380e3b
Djordje Lelek <djordje.lelek@gmail.com>
2021-01-28 01:33:19 +0100

    get and post todos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Layout from "./Layout/Layout";
-import Header from "./Header/Header";
+// import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
 
--- src/Main/ToDo/ToDo.js
+++ src/Main/ToDo/ToDo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./ToDo.module.css";
 import {
   Button,
@@ -6,17 +6,61 @@ import {
   List,
   ListItem,
   TextField,
+  Checkbox,
 } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import DeleteIcon from "@material-ui/icons/Delete";
+import axios from "axios";
+import Listaaa from "./List/List";
 
 const ToDo = () => {
-  const [todos, setTodos] = useState(["drink rakija", "danijela"]);
-  const [input, setInput] = useState("");
-  const addToDo = (event) => {
-    event.preventDefault();
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState([]);
+  const postToDos = () => {
     setTodos([...todos, input]);
-    setInput("");
+    axios
+      .post(
+        "https://to-do-app-dl-default-rtdb.firebaseio.com/todos.json",
+        input
+      )
+      .then(setInput(""));
   };
-  const list = todos.map((el) => <ListItem>{el}</ListItem>);
+  const getToDos = () => {
+    axios
+      .get("https://to-do-app-dl-default-rtdb.firebaseio.com/todos.json")
+      .then((response) => {
+        if (response.data != null)
+          setTodos([...todos, ...Object.values(response.data)]);
+      });
+  };
+  useEffect(() => {
+    getToDos();
+    postToDos();
+  }, []);
+  const list =
+    todos.length > 0
+      ? todos.map((el, id) => (
+          <ListItem key={id}>
+            {el}{" "}
+            <Button
+              variant="contained"
+              color="default"
+              className={classes.button}
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+            </Button>
+          </ListItem>
+        ))
+      : null;
   return (
     <div className={classes.ToDo}>
       <h1>Write</h1>
@@ -27,7 +71,7 @@ const ToDo = () => {
             label="✅ write a ToDo"
             type="text"
             value={input}
-            onChange={(event) => setInput(event.target.value)}
+            onChange={(event) => setInput([event.target.value])}
           />
         </FormLabel>
         <Button
@@ -35,11 +79,12 @@ const ToDo = () => {
           variant="contained"
           color="primary"
           type="submit"
-          onClick={addToDo}
+          onClick={postToDos}
         >
           Add ToDo
         </Button>
         <List className={classes.root}>{list}</List>
+        {/* <Listaaa listaaa={todos} /> */}
       </form>
     </div>
   );
