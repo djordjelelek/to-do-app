8e23793295c4efcebfe03f7f98b4f4e62480e038
Djordje Lelek <djordje.lelek@gmail.com>
2021-02-08 12:51:37 +0100

    change seassonStorage to local

--- src/Main/ToDo/Header/Header.js
+++ src/Main/ToDo/Header/Header.js
@@ -43,8 +43,8 @@ function Header(props) {
                 setLogIn(false);
                 setToken("");
                 setUserId("");
-                sessionStorage.removeItem("token");
-                sessionStorage.removeItem("userId");
+                localStorage.removeItem("token");
+                localStorage.removeItem("userId");
                 window.location.reload();
               }, 500);
             }}
